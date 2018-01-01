0812bdb3874a24378fdc85991473bbd82b4ffbf8
angrha <angrha@gmail.com>
2018-01-01 17:45:30 +0700

    add todolis, addtodo and removetodo fitur

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -2,11 +2,20 @@ new Vue({
   el: '.todoapp',
   data: {
     message: 'Todos',
-    newTodo: ''
+    newTodo: '',
+    todos: [{id: 0, title:'new todo', completed:'false'}]
   },
   methods: {
     addTodo: function(){
-      alert(this.newTodo)
+      this.todos.push({id: this.newTodo.length, title: this.newTodo, completed: 'false'})
+      this.newTodo = ''
+    },
+    removeTodo: function(todo) {
+      let index = this.todos.findIndex(x => {
+        return x === todo
+      })
+
+      this.todos.splice(index, 1)
     }
   }
 })
